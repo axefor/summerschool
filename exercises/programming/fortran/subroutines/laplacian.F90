90572827c92c2ae0b15644c7af2e0fab4e415268
axefor <axel.forslund@gmail.com>
2017-06-28 18:38:17 +0300

    implemented initialization routine

--- exercises/programming/fortran/subroutines/laplacian.F90
+++ exercises/programming/fortran/subroutines/laplacian.F90
@@ -6,12 +6,17 @@ contains
   
   subroutine initialize(field0)
 ! TODO: implement a subroutine that initializes the input array
-
+    field0(:,:)  = 65.0 ! middle
+    field0(:,1)  = 20.0 ! left
+    field0(:,ny) = 70.0 ! right
+    field0(1,:)  = 85.0 ! top
+    field0(nx,:) = 5.0  ! bottom
   end subroutine initialize
    
   subroutine laplacian(curr, prev)
 ! TODO: insert a subroutine that computes a laplacian of the
 ! array "prev" and returns it as an array "curr"
+  
   end subroutine laplacian
 
   subroutine write_field(array)
