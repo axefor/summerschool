265acce2fabf3c6d9ba63a5a4626dcc171921ef3
axefor <axel.forslund@gmail.com>
2017-06-30 21:00:34 +0300

    added better time profiling to work-sharing sum.F90

--- exercises/openmp/fortran/work-sharing/sum.F90
+++ exercises/openmp/fortran/work-sharing/sum.F90
@@ -1,22 +1,33 @@
 program vectorsum
+  use omp_lib
+  use iso_fortran_env, only: int64, real64
   implicit none
-  integer, parameter :: rk = kind(1d0)
+  integer, parameter :: rk = real64
   integer, parameter :: ik = selected_int_kind(9)
   integer, parameter :: nx = 102400
 
   real(kind=rk), dimension(nx) :: vecA, vecB, vecC
   real(kind=rk)    :: sum
+  real(kind=rk)    :: stime, ftime, stime_cpu, ftime_cpu
   integer(kind=ik) :: i
 
   ! Initialization of vectors
   do i = 1, nx
-     vecA(i) = 1.0_rk/(real(nx - i + 1, kind=rk))
-     vecB(i) = vecA(i)**2
+    vecA(i) = 1.0_rk/(real(nx - i + 1, kind=rk))
+    vecB(i) = vecA(i)**2
   end do
 
+  stime = omp_get_wtime()
+  call cpu_time(stime_cpu)
   ! TODO:
   !   Implement here the parallelized version of vector addition,
   !   vecC = vecA + vecB
+  vecC(:) = vecA(:) + vecB(:)
+
+  ftime = omp_get_wtime()
+  call cpu_time(ftime_cpu)
+  write(*,*) 'Elapsed time for vector addition: ', ftime-stime
+  write(*,*) 'CPU time for vector addition: ', ftime_cpu-stime_cpu
 
   ! Compute the check value
   write(*,*) 'Reduction sum: ', sum(vecC)
