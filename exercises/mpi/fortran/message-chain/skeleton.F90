43fdda91c138c0aa6a0d768d35a16e8f17666f30
axefor <axel.forslund@gmail.com>
2017-07-01 16:49:56 +0300

    completed mpi message chain with send/recv

--- exercises/mpi/fortran/message-chain/skeleton.F90
+++ exercises/mpi/fortran/message-chain/skeleton.F90
@@ -3,8 +3,8 @@ program basic
   use iso_fortran_env, only : REAL64
 
   implicit none
-  integer, parameter :: size = 100
-  integer :: rc, myid, ntasks, count
+  integer, parameter :: size = 100000
+  integer :: rc, myid, ntasks, count, dest, source, tag
   integer :: status(MPI_STATUS_SIZE)
   integer :: message(size)
   integer :: receiveBuffer(size)
@@ -13,7 +13,7 @@ program basic
 
   call mpi_init(rc)
   call mpi_comm_rank(MPI_COMM_WORLD, myid, rc)
-  call mpi_comm_sizeMPI_COMM_WORLD, ntasks, rc)
+  call mpi_comm_size(MPI_COMM_WORLD, ntasks, rc)
 
   message = myid
 
@@ -21,18 +21,24 @@ program basic
   call mpi_barrier(mpi_comm_world, rc)
   t0 = mpi_wtime()
 
-  ! TODO: Send and receive as defined in the assignment
+  ! Send and receive as defined in the assignment
   if ( myid < ntasks-1 ) then
+    dest = myid + 1
+    tag = 10 + myid
+    call mpi_send(message, count, MPI_INTEGER, dest, tag, MPI_COMM_WORLD, rc)
 
-     write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
-          ' Sent elements: ',size, &
-          '. Tag: ', myid+1, '. Receiver: ', myid+1
+    write(*,'(A10,I3,A20,I8,A,I3,A,I3)') 'Sender: ', myid, &
+      ' Sent elements: ',size, &
+      '. Tag: ', tag, '. Receiver: ', myid+1
   end if
 
   if ( myid > 0 ) then
+    source = myid - 1
+    tag = 9 + myid
+    call mpi_recv(receiveBuffer, count, MPI_INTEGER, source, tag, MPI_COMM_WORLD, status, rc)
 
-     write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
-          ' First element: ', receiveBuffer(1)
+    write(*,'(A10,I3,A,I3)') 'Receiver: ', myid, &
+      ' First element: ', receiveBuffer(1)
   end if
 
   ! Finalize measuring the time and print it out
@@ -40,7 +46,7 @@ program basic
   call mpi_barrier(mpi_comm_world, rc)
   call flush(6)
 
-  write(*, '(A20, I3, A, F6.3)') 'Time elapsed in rank', myid, ':', t1-t0
+  write(*, '(A20, I3, A, E12.6)') 'Time elapsed in rank', myid, ':', t1-t0
 
   call mpi_finalize(rc)
 
